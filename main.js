7b3575f81304b88e727cf69adc88b60bd3a36faa
SudhirSharmaInfo <sudhirsharma1036@gmail.com>
2025-01-17 15:16:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,25 +12,4 @@ function navclose() {
   }
 
 
-  $(document).ready(function () {
-    $("#About-mob-li").click(function() {
-        $(".About-mob-hov-box").toggle();
-    })
-
-    $("#Product-mob-li").click(function() {
-        $(".Product-mob-hov-box").toggle();
-    })
-
-    $("#Accessories-mob-li").click(function() {
-        $(".Accessories-mob-hov-box").toggle();
-    })
-
-    $("#Softwares-mob-li").click(function() {
-        $(".Softwares-mob-hov-box").toggle();
-    })
-
-    $("#Help-mob-li").click(function() {
-        $(".Help-mob-hov-box").toggle();
-    })
-
-})
+  
